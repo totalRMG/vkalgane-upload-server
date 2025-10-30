aad5f858b15c96600167ecf7a202067e3bb53d58
totalRMG <sasaomasao@mail.ru>
2025-10-31 00:12:15 +0300

    Add files via upload

--- upload_server.py
+++ upload_server.py
@@ -4,7 +4,6 @@ import boto3
 from botocore.client import Config
 import uuid
 import os
-from datetime import datetime, timedelta
 
 app = Flask(__name__)
 CORS(app)
@@ -19,8 +18,6 @@ s3 = boto3.client(
     verify=False
 )
 
-BUCKET_NAME = 'meow-test'
-
 @app.route('/')
 def home():
     return jsonify({'status': 'ok', 'message': 'Vkalgane Upload Server'})
@@ -32,47 +29,21 @@ def upload_file():
         file_extension = file.filename.split('.')[-1] if '.' in file.filename else 'jpg'
         s3_filename = f"posts/{uuid.uuid4().hex}.{file_extension}"
         
-        # Загружаем файл в S3
         s3.upload_fileobj(
             file,
-            BUCKET_NAME,
+            'meow-test',
             s3_filename,
             ExtraArgs={'ContentType': file.content_type}
         )
         
-        # Генерируем подписанный URL для доступа к файлу (действителен 1 час)
-        file_url = s3.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': BUCKET_NAME,
-                'Key': s3_filename
-            },
-            ExpiresIn=3600
-        )
+        file_url = f"https://meow-test.s3.ru-1.storage.selcloud.ru/{s3_filename}"
         
         return jsonify({
             'success': True,
             'url': file_url,
-            'filename': file.filename,
-            's3_key': s3_filename
+            'filename': file.filename
         })
         
-    except Exception as e:
-        return jsonify({'success': False, 'error': str(e)}), 500
-
-@app.route('/get-signed-url/<path:s3_key>')
-def get_signed_url(s3_key):
-    """Генерирует новый подписанный URL для файла"""
-    try:
-        file_url = s3.generate_presigned_url(
-            'get_object',
-            Params={
-                'Bucket': BUCKET_NAME,
-                'Key': s3_key
-            },
-            ExpiresIn=3600
-        )
-        return jsonify({'success': True, 'url': file_url})
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)}), 500
 
